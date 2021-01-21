9587aebb14bddced0bc1ecfd9f289c7034cb285c
Prabhakar Kumar <prabhakar1069@gmail.com>
2021-01-21 14:51:20 +0530

    Create Log In functionality in Nav Bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
-  get 'homme/index'
+  # get 'home/index'
+  # get 'homme/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
